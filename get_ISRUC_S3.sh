e526b3309c8dec532964dc1757ea13d91487aa2c
XiaojunNing <42231551+NingXJ1999@users.noreply.github.com>
2021-09-24 00:08:39 +0800

    Update get_ISRUC_S3.sh
    fix a bug of raw data path

--- get_ISRUC_S3.sh
+++ get_ISRUC_S3.sh
@@ -1,8 +1,8 @@
 mkdir -p ./data/ISRUC_S3/ExtractedChannels
-mkdir -p ./data/ISRUC_S3/Data
+mkdir -p ./data/ISRUC_S3/RawData
 echo 'Make data dir: ./data/ISRUC_S3'
 
-cd ./data/ISRUC_S3/Data
+cd ./data/ISRUC_S3/RawData
 for s in {1..10}:
 do
     wget http://dataset.isr.uc.pt/ISRUC_Sleep/subgroupIII/$s.rar
